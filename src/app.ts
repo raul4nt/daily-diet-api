53964e685174a16b360f152cccabe41b1148df5e
Raul Silveira <raulantsilveira@gmail.com>
2024-10-21 13:55:35 -0300

    fix: convert line endings to LF

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 
 export const app = fastify()
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import { app } from './app'
 import { env } from './env'
 
-app 
+app
   .listen({
     port: env.PORT,
   })
   .then(() => {
-    console.log(`HTTP Server Running on port ${env.PORT}`);
+    console.log(`HTTP Server Running on port ${env.PORT}`)
   })
