35e1c2a1798228a3d84839a0899af9a424c74163
Raul Silveira <raulantsilveira@gmail.com>
2024-10-21 16:01:29 -0300

    define tables type

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,24 @@
+import { Knex } from "knex";
+
+declare module 'knex/types/tables' {
+    export interface Tables {
+      users: {
+        id: string 
+        email: string 
+        password: string 
+        created_at: string 
+      },
+
+      meals: {
+        id: string 
+        user_id: string 
+        name: string 
+        description: string 
+        date: Date 
+        time: Date 
+        isInDiet: boolean
+      }
+
+    }
+}
+
